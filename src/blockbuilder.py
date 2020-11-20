4721577fbc33a73aec356614551f0a101c0a3677
Murch <murch@murch.one>
2020-11-20 11:01:47 -0500

    Add getRepresentativeTx

--- src/blockbuilder.py
+++ src/blockbuilder.py
@@ -1,5 +1,10 @@
 import json
 
+def getRepresentativeTx(transactions):
+    txids = transactions.keys()
+    txids.sort()
+    return txids[0]
+
 with open('../data/mempool.json') as f:
     mempool = json.load(f)
 
@@ -8,5 +13,13 @@ print(len(mempool))
 toBeClustered = {txid: vals for txid, vals in mempool.items() if vals["ancestorcount"] + vals["descendantcount"] > 2}
 
 print(len(toBeClustered))
+
+testDict = {
+    "acb":{"key":"otherValue"},
+    "123":{"key":"value"},
+    "abc":{"key":"v3"},
+}
+
+assert getRepresentativeTx(testDict) == "123", "Should be 123"
 
 f.close()
