458c71742d95d636ba46c6efb80b898dc00b5771
Murch <murch@murch.one>
2020-12-02 22:06:13 -0500

    Add clustering of transactions

--- blockbuilder.py
+++ blockbuilder.py
@@ -25,6 +25,21 @@ def clusterTransaction(txid, transaction):
 with open('data/mempool.json') as f:
     mempool = json.load(f)
 
+for txid in mempool.keys():
+    txClusterMap[txid] = txid
+
 toBeClustered = {txid: vals for txid, vals in mempool.items() if vals["ancestorcount"] + vals["descendantcount"] > 2}
+anyUpdated = True
+
+while (anyUpdated):
+    anyUpdated = False
+    for txid, vals in mempool.items():
+        repBefore = txClusterMap[txid]
+        print "repBefore: ", repBefore
+        clusterTransaction(txid, vals)
+        repAfter = txClusterMap[txid]
+        print "repAfter: ", repAfter
+        anyUpdated = anyUpdate or repAfter != repBefore
+        print "anyUpdated: ", anyUpdated
 
 f.close()
