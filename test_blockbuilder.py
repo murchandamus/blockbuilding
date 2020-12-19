b4415dbf8c7f1f07e59663ebf8fcdc76af63d2f6
Murch <murch@murch.one>
2020-12-19 10:35:47 -0500

    Fix formatting of test file to conform w/ synastic

--- test_blockbuilder.py
+++ test_blockbuilder.py
@@ -2,20 +2,30 @@ import unittest
 import blockbuilder
 
 testDict = {
-    "abc":{"key":"otherValue", "depends": ["123"], "spentby":[]},
-    "123":{"key":"value", "depends": [], "spentby":["abc"]},
-    "nop":{"key":"v4", "depends": [], "spentby":["qrs"]},
-    "qrs":{"key":"baum", "depends": ["nop"], "spentby":["tuv"]},
-    "tuv":{"key":"irgendwas", "depends": ["qrs"], "spentby":[]},
-    "xyz":{"key":"v3", "depends": [], "spentby":[]},
+    "abc": {"key": "otherValue", "depends":  ["123"], "spentby": []},
+    "123": {"key": "value", "depends":  [], "spentby": ["abc"]},
+    "nop": {"key": "v4", "depends":  [], "spentby": ["qrs"]},
+    "qrs": {"key": "baum", "depends":  ["nop"], "spentby": ["tuv"]},
+    "tuv": {"key": "irgendwas", "depends":  ["qrs"], "spentby": []},
+    "xyz": {"key": "v3", "depends":  [], "spentby": []},
 }
+
 
 class TestBlockbuilder(unittest.TestCase):
     def test_get_representative_tx(self):
-        self.assertEqual(blockbuilder.getRepresentativeTxid("qrs", testDict["qrs"]), "nop", "Should be nop")
+        self.assertEqual(
+                blockbuilder.getRepresentativeTxid("qrs", testDict["qrs"]),
+                "nop",
+                "Should be nop"
+            )
 
     def getLocalClusterTxids(self):
-        self.assertEqual(blockbuilder.getLocalClusterTxids("abc", testDict["abc"]) == ["abc", "123"], "Should be ['xyz', '123']")
+        self.assertEqual(
+                blockbuilder.getLocalClusterTxids("abc", testDict["abc"]),
+                ["abc", "123"],
+                "Should be ['xyz', '123']"
+            )
+
 
 if __name__ == '__main__':
     unittest.main()
