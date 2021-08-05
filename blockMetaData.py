df5a24d6d4021a55989e5f3cf563d560fe38b3fb
Murch <murch@murch.one>
2021-08-05 16:23:20 -0400

    Get block and tx data from API

--- blockMetaData.py
+++ blockMetaData.py
@@ -1,62 +1,22 @@
-from bs4 import BeautifulSoup
-import urllib.request
-import urllib.error
-import ssl
-import sys
-import multiprocessing
+import requests
 
-def readUrl(Id, type):
-    if type=='block':
-        url = r"https://www.blockchain.com/btc/block/" + str(Id)
-    elif type=='tx':
-        url = r"https://www.blockchain.com/btc/tx/" + str(Id)
-    ssl._create_default_https_context = ssl._create_unverified_context  # specific for mac issues
-    try:
-        fp = urllib.request.urlopen(url)
-        mybytes = fp.read()
-        wsHtml = mybytes.decode("utf8")
-        fp.close()
-        soup = BeautifulSoup(wsHtml, 'html.parser')
-    except urllib.error.HTTPError:
-        print(str(Id)+": Block not found ")
-        pass
-    except:
-        print(str(Id)+" Unexpected error:", sys.exc_info()[0])
-        pass
-    return soup
+def getBlockInfo(blockId):
+    url = r"https://blockstream.info/api/block/" + str(blockId)
+    blockdata = requests.get(url).json()
+    # print(str(blockdata))
+    print('height: ' + str(blockdata['height']))
+    print('time: ' + str(blockdata['timestamp']))
+    return blockdata
 
-
-def getBlockDateTime(blockId):
-
-    timestamp_string = readUrl(blockId, 'block').find("div", string="Timestamp").findNext('div').contents[0].contents[0]
-    date, time = timestamp_string.split(' ')
-    print(date)
-    return date, time
-
-def getBlockHeight(blockId):
-    try:
-        height_string = readUrl(blockId, 'block').find("div", string="Height").findNext('div').findNext('div').contents[0].contents[0]
-    except:
-        height_string='-1'
-        pass
-    return int(height_string)
-
-
-
-def getTxSize(txId):
-    try:
-        weightOfTx = readUrl(txId, 'tx').find("div", string='Size').findNext("div").findNext('div').contents[0].contents[0]
-        if weightOfTx.find("bytes")!=-1:
-            weightOfTx = weightOfTx[0:weightOfTx.find("bytes")-1]
-        return weightOfTx
-    except:
-        print('tx not found '+ txId, sys.exc_info()[0])
-        pass
+def getTxWeight(txid):
+    url = r"https://blockstream.info/api/tx/" + str(txid)
+    txdata = requests.get(url).json()
+    # print(str(txdata))
+    print('weight: ' + str(txdata['weight']))
+    return txdata['weight']
 
 if __name__ == '__main__':
-
     block = '00000000000000000010627e30472395f6b9a006f1784be048e40a48418ab8c7'
     tx = 'cae8f7930fef5704a8a55c19e5d52c3d7257ad5edec853e0729cd1db5d377a3b'
-    print(getTxSize(tx))
-    print(getBlockDateTime(block))
-    print(getBlockHeight(block))
+    getBlockInfo(block)
+    getTxWeight(tx)
--- preprocessesForMonthBuilder.py
+++ preprocessesForMonthBuilder.py
@@ -1,19 +1,27 @@
 import os
 import blockMetaData as md
 
-
 def addBlockHeightToFileName(fileLocation, fileName, height):
     if len(height) < 6:
         height = '0'*(6-len(height))+height
     os.rename(fileLocation+r'/'+fileName, fileLocation+ r'/' + height +'_' +fileName)
 
-
 def addBlockHeightForDirectory(directory):
     relvantPathEndings = ['.mempool', '.block', '.gbt']
+    blockHeights = {}
     for file in os.listdir(directory):
         if file.find("_") == -1 and any(x in file for x in relvantPathEndings):
-            height = str(md.getBlockHeight(file[0:file.find(r'.')]))
-            addBlockHeightToFileName(directory, file, height)
+            blockId = file[0:file.find(r'.')]
+            height = -1
+            if blockId in blockHeights:
+                height = blockHeights[blockId]
+            else:
+                blockData = str(md.getBlockInfo(blockId))
+                height = blockData['height']
+                blockHeights[blockId] = height
+            if height < 0:
+                raise Exception("height for " + blockId + " not found")
+            addBlockHeightToFileName(directory, file, str(height))
 
 def createAllowListFile(directory, resultFile):
     print("start set")
@@ -46,9 +54,8 @@ def getCoinbaseSizes(directory):
             with open(os.path.join(directory, file), 'r') as import_file:
                 import_file.readline()
                 coinbaseTxId = import_file.readline()
-                coinbaseSizeDict[blockNum] = md.getTxSize(coinbaseTxId)
+                coinbaseSizeDict[blockNum] = md.getTxWeight(coinbaseTxId)
     return coinbaseSizeDict
-
 
 if __name__ == '__main__':
     directory = "/Users/clara/Documents/GitHub/blockbuilding/data/data_example/test"
