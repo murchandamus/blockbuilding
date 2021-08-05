c0c9c58f60f26b669aedc9c92d0284238acde363
ClaraShk <33697523+ClaraShk@users.noreply.github.com>
2021-08-05 11:18:51 +0300

    Added function loading a new mempool for a new block

--- monthBuilder.py
+++ monthBuilder.py
@@ -1,9 +1,9 @@
-import blockbuilder
+import blockbuilder as bb
 import os
 from os.path import isfile, join
 
 # TODO:
-## Identify the lowest block-id (i.e. height)
+## Identify the lowest block-id (i.e. height) - done
 ## load mempool of for the first block
 ## filter to txs in whitelist
 ## build block respecting the coinbase size
@@ -19,16 +19,15 @@ from os.path import isfile, join
 class Monthbuilder():
     def __init__(self, monthPath):
         self.pathToMonth = monthPath
-        self.globalMempool = blockbuilder.Mempool()
+        self.globalMempool = bb.Mempool()
         self.confirmedList = set()
         self.allowList = set()
+        self.usedTxList = set()
         self.height = -1
 
     def loadAllowList(self):
         files = os.listdir(self.pathToMonth)
-        print('Looking for allowList')
         for f in files:
-            print('file in folder: ' + f)
             if f.endswith('.allow'):
                 with open(os.path.join(self.pathToMonth, f), 'r') as import_allow_list:
                     for line in import_allow_list:
@@ -37,17 +36,44 @@ class Monthbuilder():
         if len(self.allowList) == 0:
             raise ValueError('Allowed list empty')
 
+
+    def updateUsedList(self, txList):
+        newTxSet = set(txList)
+        self.usedTxList = self.usedTxList.union(newTxSet)
+
+    def loadBlockMempool(self, blockId):
+        FileFound = 0
+        for file in os.listdir(self.pathToMonth):
+            if file.endswith(blockId+'.mempool'):
+                FileFound = 1
+                tempMempool = bb.Mempool()
+                print(os.path.join(self.pathToMonth, file))
+                tempMempool.fromTXT(os.path.join(self.pathToMonth, file))
+                print('size of mempool is: '+str([k for k in tempMempool.txs.keys()]))
+                tempMempoolSet = set([k for k in tempMempool.txs.keys()])
+                print(tempMempoolSet)
+                tempMempoolSet.update([k for k in self.globalMempool.txs.keys()])
+                print(tempMempoolSet)
+                tempMempoolSet.difference_update(self.usedTxList)
+                print(tempMempoolSet)
+                tempMempoolSet.intersection_update(self.allowList)
+                print("tamp mempool set is: ")
+                print(tempMempoolSet)
+        if FileFound == 0:
+            raise FileNotFoundError("Mempool not found")
+
+
     def getNextBlockHeight(self):
         ## Assume that there are mempool files in folder and they are prefixed with a _seven_ digit block height
         if self.height > 0:
-            height = height+1
-            return height
+            self.height = self.height+1
+            return self.height
         else:
             dirContent = os.listdir(self.pathToMonth)
             onlymempool =  [f for f in dirContent if f.endswith('.mempool')]
             onlymempool.sort()
-            height = onlymempool[0][0:7]
-            return height
+            self.height = onlymempool[0][0:7]
+            return self.height
 
 def main(argv):
     mempoolfilepath = ''
@@ -78,4 +104,6 @@ def main(argv):
     print('Elapsed time: ' + str(endTime - startTime))
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    mb = Monthbuilder("./data/data_example/test")
+    mb.loadAllowList()
+    mb.loadBlockMempool('123456')
