39eb494cb337c5e51b7a26a39736947ae924c334
Murch <murch@murch.one>
2021-09-28 10:20:03 -0400

    Rename ancestorBlockBuilder

--- ancestorBlockBuilder.py
+++ ancestorBlockBuilder.py
@@ -1,7 +1,6 @@
 import blockbuilder as bb
 from collections import OrderedDict
 
-
 class BlockbuilderByAnces():
     def __init__(self, mempool, weightLimit=3992820):
         self.mempool = mempool
@@ -34,12 +33,12 @@ class BlockbuilderByAnces():
             self.mempool.txs[tx].ancestors = self.getAncestors(self.mempool.txs[tx], 0)
             self.mempool.txs[tx].ancestor_fee = sum([self.mempool.txs[ancestor_txid].fee for ancestor_txid in self.mempool.txs[tx].ancestors])
             self.mempool.txs[tx].ancestor_weight = sum([self.mempool.txs[ancestor_txid].weight for ancestor_txid in self.mempool.txs[tx].ancestors])
-            self.mempool.txs[tx].feerate = self.mempool.txs[tx].ancestor_fee / self.mempool.txs[tx].ancestor_weight
+            self.mempool.txs[tx].ancestor_feerate = self.mempool.txs[tx].ancestor_fee / self.mempool.txs[tx].ancestor_weight
             for anc in [self.mempool.txs[a].txid for a in self.mempool.txs[tx].ancestors if self.mempool.txs[a].txid != self.mempool.txs[tx].txid]:
                 txs[anc].descendants.append(tx)
 
             # Reorder transactions by ancestor_feerate
-        return OrderedDict(sorted(txs.items(), key=lambda tx: tx[1].feerate, reverse=True))
+        return OrderedDict(sorted(txs.items(), key=lambda tx: tx[1].ancestor_feerate, reverse=True))
 
 
     def buildBlockTemplat(self, weightlimit):
@@ -80,7 +79,7 @@ class BlockbuilderByAnces():
                 block.append(txs.pop(self.mempool.txs[ancestor].txid).txid)
                 block_weight += self.mempool.txs[ancestor].weight
             # Resort list
-            txs = OrderedDict(sorted(txs.items(), key=lambda tx: tx[1].feerate, reverse=True))
+            txs = OrderedDict(sorted(txs.items(), key=lambda tx: tx[1].ancestor_feerate, reverse=True))
             continue  # Break into outer loop. Restart iteration over all unincluded txs
 
         return block
