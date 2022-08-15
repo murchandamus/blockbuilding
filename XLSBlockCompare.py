31e22cb65cd6d2e5afb5401ee07f3c429410b305
ClaraShk <33697523+ClaraShk@users.noreply.github.com>
2022-08-15 16:30:37 -0400

    out put sum to XLS by height

--- XLSBlockCompare.py
+++ XLSBlockCompare.py
@@ -7,6 +7,7 @@ class Block():
         self.weights = weightsDic
         self.totalFees = totalFeeDic
 
+
 def getBlockDetailsFromFile(fileLocation):
     try:
         blockDetails = open(fileLocation, 'r')
@@ -17,14 +18,17 @@ def getBlockDetailsFromFile(fileLocation):
         blockDetails.close()
         return totalFee, weight
     except FileNotFoundError:
-        print("No such file")
+        print("No such file to get from "+str(fileLocation))
     except:
         print("some thing else is wrong with " + str(fileLocation))
 
+
 def getBlockNumbersAndTypes(directory = r"./blockCompareTest"):
     files = os.listdir(directory)
-    fileTypes = list(dict.fromkeys([f[f.find("."):] for f in files]))
-    allowedBlockTypes = ['.gbt', '.block', '.byclusters', '.LpSolve']
+    fileTypes = list(dict.fromkeys([f[f.rfind("."):] for f in files]))
+    print('file types:')
+    print(fileTypes)
+    allowedBlockTypes = ['.gbt', '.block', '.byclusters', '.LpSolve', '.byancestors']
     blockTypes = list(set(fileTypes) & set(allowedBlockTypes))
     blockIdSet = set()
     for f in files:
@@ -33,6 +37,20 @@ def getBlockNumbersAndTypes(directory = r"./blockCompareTest"):
     blockNumbers = list(blockIdSet)
     return blockNumbers, blockTypes
 
+
+def getBlockHeightsAndTypes(directory):
+    files = os.listdir(directory)
+    fileTypes = list(dict.fromkeys([f[f.rfind("."):] for f in files]))
+    print('file types:')
+    print(fileTypes)
+    allowedBlockTypes = ['.gbt', '.block', '.byclusters', '.LpSolve', '.byancestors']
+    blockTypes = list(set(fileTypes) & set(allowedBlockTypes))
+    blockIdSet = set()
+    for f in files:
+        if f.endswith(tuple(blockTypes)):
+            blockIdSet.add(f[:f.find('-')])
+    blockHeights = list(blockIdSet)
+    return blockHeights, blockTypes
 
 def createBlockDic(blockNumbers, filetypes):
     blocks={}
@@ -48,10 +66,28 @@ def createBlockDic(blockNumbers, filetypes):
             blocks[block] = Block(str(block), weights, fees)
     return blocks
 
+
+def createBlockDicByHeight(dir, heights):
+    blocks={}
+    for height in heights:
+        file = os.path.join(dir, [filename for filename in os.listdir(dir) if filename.startswith(str(height))][0])
+        try:
+            fee, weight = getBlockDetailsFromFile(file)
+            type = file[file.rfind("."):]
+        except TypeError:
+            print("No such file by height: " + str(file)+ " "+str(height))
+        blocks[height] = {"weight": weight, "fee": fee, "type": type}
+        print('weight ' + str(weight))
+    return blocks
+
+
 def writeBlockDetailsToXSL(xlsFileName, blockDetails, blockTypes):
     print("file name is "+str(xlsFileName))
+    print("BlockDic: "+str(blockDetails))
+    print("Types: "+str(blockTypes))
     wb = Workbook()
     sheet1 = wb.add_sheet('Results', cell_overwrite_ok=True)
+
     inputs = blockDetails[next(iter(blockDetails))].weights.keys()
     line = 0
     k = 1
@@ -82,16 +118,41 @@ def writeBlockDetailsToXSL(xlsFileName, blockDetails, blockTypes):
         line += 1
     wb.save(xlsFileName)
 
+
+
+def writeBlockDetailsToXSLByHeight(xlsFileName, blocks):
+    print("printing by Height")
+    wb = Workbook()
+    sheet1 = wb.add_sheet('Results', cell_overwrite_ok=True)
+    sheet1.write(0, 0, "height")
+    sheet1.write(0, 1, "weight")
+    sheet1.write(0, 2, "fee")
+    sheet1.write(0, 3, "type")
+    line = 1
+    for height in blocks.keys():
+        sheet1.write(line,0,height)
+        sheet1.write(line, 1, blocks[height]["weight"])
+        sheet1.write(line, 2, blocks[height]["fee"])
+        sheet1.write(line, 3, blocks[height]["type"])
+        line += 1
+    wb.save(xlsFileName)
+
+
+    return 0
+
+
+
 if __name__ == '__main__':
 #    directory = input("which directory? ")
 #    OutputFile = input("where to send results? ")
     directory = input("dir?")
     OutputFile = input("output?")
-    blockNum, blockTypes = getBlockNumbersAndTypes(directory)
-    print(blockTypes)
-    BlockDic = createBlockDic(blockNum, blockTypes)
+#    blockNum, blockTypes = getBlockNumbersAndTypes(directory)
+    blockHeights, blockTypes = getBlockHeightsAndTypes(directory)
+    print(blockHeights)
+#    BlockDic = createBlockDic(blockHeights, blockTypes)
+    BlockDic = createBlockDicByHeight(directory, blockHeights)
     for block in BlockDic:
-        print(BlockDic[block].blockNumber)
-        print(BlockDic[block].weights)
-        print(BlockDic[block].totalFees)
-    writeBlockDetailsToXSL(OutputFile, BlockDic, blockTypes)
+        print(BlockDic[block])
+    writeBlockDetailsToXSLByHeight(OutputFile, BlockDic)
+#    writeBlockDetailsToXSL(OutputFile, BlockDic, blockTypes)
