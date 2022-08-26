256d1e5c30b59924739de4f027fd0a0237d579ba
ClaraShk <33697523+ClaraShk@users.noreply.github.com>
2022-08-26 16:39:06 -0400

    Summarize month simulations
    In a month worth of files, go over each folder of experiment and summarize results of all runs into one file

---
+++ process_batched_sim.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import os
+import csv
+
+class Summery():
+    def __init__(self, sim_id, keys=['total fees', 'average fee', 'median fee', 'var of fee']):
+        self.id = sim_id
+        self.blockInSim = -1
+        self.blocksByCluster = -1
+        self.keys = keys
+        self.resCluster = dict.fromkeys(self.keys, -1)
+        self.resAncestor = dict.fromkeys(self.keys, -1)
+        self.resDiff = dict.fromkeys(self.keys, -1)
+
+
+
+
+
+
+
+
+def find_stats(res, baseline, sim_id):
+    summ = Summery(sim_id)
+    summ.blockInSim = res.shape[0]
+    joinedRes = res.join(baseline, lsuffix='_res', rsuffix='_baseline')
+    print('res:')
+    print(res['fee'])
+    print('joined table, fee_res')
+    print(joinedRes['fee_res'])
+    f_data = joinedRes[joinedRes['type_res'] =='.byclusters']
+    summ.blocksByCluster = f_data.shape[0]
+    print(f_data['fee_baseline'])
+    summ.resCluster['total fees'] = f_data['fee_res'].sum()
+    summ.resCluster['average fee'] = f_data['fee_res'].mean()
+    summ.resCluster['median fee'] = f_data['fee_res'].median()
+    summ.resCluster['var of fee'] = f_data['fee_res'].var()
+    summ.resAncestor['total fees'] = f_data['fee_baseline'].sum()
+    summ.resAncestor['average fee'] = f_data['fee_baseline'].mean()
+    summ.resAncestor['median fee'] = f_data['fee_baseline'].median()
+    summ.resAncestor['var of fee'] = f_data['fee_baseline'].var()
+    for key in summ.resCluster.keys():
+        summ.resDiff[key] = summ.resCluster[key] - summ.resAncestor[key]
+    return summ
+
+
+def find_res_file_in_folder(folder):
+    for file in os.listdir(folder):
+        if file.endswith('.csv'):
+            return os.path.join(folder, file)
+
+
+def summ_exp(folder, baselineDir):
+    exp_dict = {}
+    baseline_data = pd.read_csv(find_res_file_in_folder(baselineDir))
+    for exp_folder in os.listdir(folder):
+        d = os.path.join(folder,exp_folder)
+        if os.path.isdir(d):
+            res = pd.read_csv(find_res_file_in_folder(d))
+            exp_dict[exp_folder] = find_stats(res, baseline_data, exp_folder)
+    return exp_dict
+
+
+def sum_month(month_folder):
+    baseline_folder = os.path.join(month_folder, 'baseline')
+    for subfolder in os.listdir(month_folder):
+        if os.path.isdir(os.path.join(month_folder, subfolder)) and ('baseline' not in subfolder):
+            exp_summery = summ_exp(os.path.join(month_folder, subfolder), baseline_folder)
+            print_to_file(os.path.join(month_folder, 'res_'+str(subfolder)+'.csv'), exp_summery)
+
+
+def print_to_file(file_path, dict):
+    with open(file_path, 'w', newline='') as csv_file:
+        header = ['id','blocks in sim', 'blocks by Cluster'] + [x+'_byCluster' for x in keys] + [x+'_byAncestor' for x in keys] + [x+'_diff' for x in keys]
+        writer = csv.writer(csv_file)
+        writer.writerow(header)
+        for res in dict.keys():
+            row = [dict[res].id, dict[res].blockInSim, dict[res].blocksByCluster]+list(dict[res].resCluster.values())+list(dict[res].resAncestor.values())+list(dict[res].resDiff.values())
+            writer.writerow(row)
+    csv_file.close()
+
+
+if __name__ == '__main__':
+    keys = ['total fees', 'average fee', 'median fee', 'var of fee']
+    sum_month('data/Zepril 76')
